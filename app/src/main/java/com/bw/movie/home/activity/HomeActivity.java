2db833c3c5755d44a0d879e37abd631e0479c0e0
Administrator <928873848@qq.com>
2019-01-24 20:24:54 +0800

    31

---
+++ app/src/main/java/com/bw/movie/home/activity/HomeActivity.java
@@ -0,0 +1,33 @@
+package com.bw.movie.home.activity;
+
+import android.os.Bundle;
+
+import com.bw.movie.R;
+import com.bw.movie.base.BaseActivty;
+
+public class HomeActivity extends BaseActivty {
+    @Override
+    protected void onNetSuccess(Object data) {
+
+    }
+
+    @Override
+    protected void onNetFail(String error) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_home;
+    }
+}
