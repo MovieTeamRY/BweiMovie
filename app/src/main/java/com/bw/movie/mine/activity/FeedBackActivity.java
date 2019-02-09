aedfc8ca67b322a10ef7db185420b4a0e86158b0
xieyu <2814874857@qq.com>
2019-02-09 16:12:27 +0800

    72

---
+++ app/src/main/java/com/bw/movie/mine/activity/FeedBackActivity.java
@@ -0,0 +1,36 @@
+
+package com.bw.movie.mine.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bw.movie.R;
+import com.bw.movie.base.BaseActivty;
+
+public class FeedBackActivity extends BaseActivty {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_feed_back;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onNetSuccess(Object data) {
+
+    }
+
+    @Override
+    protected void onNetFail(String error) {
+
+    }
+}
