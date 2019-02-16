c8f2fb11b0ca9fa8e1810431e56f337bdcabf626
Administrator <928873848@qq.com>
2019-02-16 12:20:18 +0800

    84

--- app/src/main/java/com/bw/movie/base/BaseActivty.java
+++ app/src/main/java/com/bw/movie/base/BaseActivty.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 
@@ -19,6 +20,10 @@ import com.bw.movie.mvp.view.Iview;
 import com.bw.movie.utils.ToastUtil;
 import com.bw.movie.view.CircularLoading;
 import com.squareup.leakcanary.RefWatcher;
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
+import com.tencent.android.tpush.common.Constants;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -46,6 +51,30 @@ public abstract class BaseActivty extends AppCompatActivity implements Iview {
         initView(savedInstanceState);
         //添加数据
         initData();
+        XGPushManager.registerPush(getApplicationContext(),
+                new XGIOperateCallback() {
+                    @Override
+                    public void onSuccess(Object data, int flag) {
+                        Log.w(Constants.LogTag, "+++ register push sucess. token:" + data + "flag" + flag);
+
+                      /*  m.obj = "+++ register push sucess. token:" + data;
+                        m.sendToTarget();*/
+                    }
+
+                    @Override
+                    public void onFail(Object data, int errCode, String msg) {
+                        Log.w(Constants.LogTag,
+                                "+++ register push fail. token:" + data
+                                        + ", errCode:" + errCode + ",msg:"
+                                        + msg);
+                       /* m.obj = "+++ register push fail. token:" + data
+                                + ", errCode:" + errCode + ",msg:" + msg;
+                        m.sendToTarget();*/
+                    }
+                });
+
+        // 获取token
+        XGPushConfig.getToken(this);
     }
     /**
      * 注销页面 解绑
--- app/src/main/java/com/bw/movie/base/MyApplication.java
+++ app/src/main/java/com/bw/movie/base/MyApplication.java
@@ -5,12 +5,17 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.util.Log;
+
 
 import com.facebook.cache.disk.DiskCacheConfig;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
 
 public class MyApplication extends Application {
 
@@ -47,5 +52,30 @@ public class MyApplication extends Application {
         }
         //检测内存险漏
         refWatcher = LeakCanary.install(this);
+       /* //信鸽推送
+        XGPushConfig.enableDebug(this,true);
+        XGPushConfig.enableOtherPush(getApplicationContext(), true);
+        XGPushConfig.setHuaweiDebug(true);
+        XGPushConfig.setMiPushAppId(getApplicationContext(), "d0f47e3bdce83");
+        XGPushConfig.setMiPushAppKey(getApplicationContext(), "fd195eb6a19f52d6a9e5c77f517a908b");
+        XGPushConfig.setMzPushAppId(this, "d0f47e3bdce83");
+        XGPushConfig.setMzPushAppKey(this, "fd195eb6a19f52d6a9e5c77f517a908b");
+        XGPushManager.registerPush(this, new XGIOperateCallback() {
+            @Override
+            public void onSuccess(Object data, int flag) {
+                //token在设备卸载重装的时候有可能会变
+                Log.i("TPush", "注册成功，设备token为：" + data);
+            }
+            @Override
+            public void onFail(Object data, int errCode, String msg) {
+                Log.i("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+            }
+        });*/
     }
+/*
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        StubAppUtils.attachBaseContext(context);
+    }*/
 }
